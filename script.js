5ddaad8657e16c149825f2490c31a5eaf86dcdb7
DanielKalistratov <amphipothekus@gmail.com>
2022-02-05 21:52:32 +0100

    Darkmode

---
+++ script.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body")
+      navbar = body.querySelector(".navbar")
+      modeSwitch = body.querySelector(".mode")
+
+      modeSwitch.addEventListener("click", () =>{
+          body.classList.toggle("dark");
+      });
