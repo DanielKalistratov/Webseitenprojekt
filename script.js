c208c79db694ddda67b9237c7895dacc8912ce98
DanielKalistratov <amphipothekus@gmail.com>
2023-01-07 17:56:02 +0100

    Erste version kompatibel auf Handy

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* dark7whitemode */
+
 const body = document.querySelector("body")
 navbar = body.querySelector(".navbar")
 modeSwitch = body.querySelector(".mode")
@@ -6,3 +8,13 @@ modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
 
 });
+
+/* nevbar mobil */
+
+const handyline = document.querySelector('.handyline')
+const navlinks = document.querySelector('.menu-links')
+const links = document.querySelectorAll('.menu-links li')
+
+handyline.addEventListener('click', () =>{
+    navlinks.classList.toggle("open")
+})
